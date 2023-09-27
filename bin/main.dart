de085278f22e4c8381c0c0b6d013a153f3f73a7f
JotaveX <jvpiloni@gmail.com>
2023-09-27 04:05:29 -0300

    Feature: Recursive backup code

--- bin/main.dart
+++ bin/main.dart
@@ -1,40 +1,36 @@
 import 'dart:io';
 
 void main() {
-  final sourceDir = Directory('C:/Users/jvpil/Desktop/idle_master_extended_v1.10.0/');
+  final sourceDir = Directory('C:\\Users\\jvpil\\OneDrive\\Documentos\\Github\\BackupSys\\TesteBackup\\');
   final destDir = Directory('C:/Users/jvpil/OneDrive/Documentos/Github/BackupSys/backup/');
 
-    if (!sourceDir.existsSync()) {
-        print('O diretório de origem não existe.');
-        return;
-    }
+  if (!sourceDir.existsSync()) {
+    print('O diretório ${sourceDir} de origem não existe.');
+    return;
+  }
 
-    if (!sourceDir.statSync().modeString().contains('r')) {
-        print('Sem permissão de leitura para o diretório de origem.');
-        return;
-    }
+  if (!sourceDir.statSync().modeString().contains('r')) {
+    print('Sem permissão de leitura para o diretório de origem.');
+    return;
+  }
 
-    if (!destDir.existsSync()) {
-        destDir.createSync(recursive: true);
-    }
+  if (!destDir.existsSync()) {
+    destDir.createSync(recursive: true);
+  }
 
-    copyDirectory(sourceDir, destDir);
+  copyDirectory(sourceDir, destDir);
 
-    print('Backup realizado com sucesso.');
+  print('Backup realizado com sucesso.');
 }
 
 void copyDirectory(Directory source, Directory destination) {
     source.listSync().forEach((entity) {
-        if (entity is File) {
-            final fileName = entity.path.split('/').last;
-            final destFile = File('${destination.path}/$fileName');
-            print('Copying ${entity.path} to ${destFile.path}');
-            entity.copySync(destFile.path);
-        } else if (entity is Directory) {
-            final subDir = Directory('${destination.path}/${entity.path.split('/').last}');
-            print(subDir);
-            subDir.createSync();
-            copyDirectory(entity, subDir);
-        }
+      if (entity is Directory) {
+        final newDirectory = Directory('${destination.path}/${entity.path.split('\\').last}');
+        newDirectory.createSync();
+        copyDirectory(entity.absolute, newDirectory.absolute);
+      } else if (entity is File) {
+        entity.copySync('${destination.path}/${entity.path.split('\\').last}');
+      }
     });
 }
